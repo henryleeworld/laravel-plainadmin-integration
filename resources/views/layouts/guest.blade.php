5012fbb76447d75b4e55393da33c8de14cd5b956
Henry Lee <henryleeworld@gmail.com>
2024-01-09 00:21:52 +0800

    feat:更新最新資料

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,12 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    @vite('resources/sass/app.scss')
+    @vite(['resources/sass/app.scss'])
 </head>
 <body>
 
 <div class="row g-0 auth-row">
     @yield('content')
 </div>
+@vite(['resources/js/app.js'])
 </body>
 </html>
